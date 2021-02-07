1416bbd44ff18e0cf8276a3b4c6dd85609a8d5ec
AzretRealme <azretovichh@gmail.com>
2021-02-07 18:14:58 +0600

    addeddd

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,17 @@ import static parent.Rank.*;
 public class Main {
     public static void main(String[] args){
 
-            Developer[] developers = new Developer[]{new Developer("I am Junior Developer", JUNIOR),
-                    new Developer("I am Senior Developer", SENIOR),
-                    new Developer("I am Middle Developer", MIDDLE),
-                    new Developer("I am Teamlead Developer", TEAMLEAD)};
+        Developer[] developers = new Developer[]{new Developer(JUNIOR, "Anthony", 27, 33000),
+                new Developer(MIDDLE, "Williams", 26, 62000),
+                new Developer(SENIOR, "Kane", 28, 86000),
+                new Developer(TEAMLEAD, "Alex", 30, 53000)};
+
+        for (Developer develop : developers) {
+            develop.writeCode();
+            develop.goToDayOff();
+            develop.goToVacation();
+        }
+
             printDeveloperGrade(developers);
         }
         public  static void printDeveloperGrade(Developer[] developers) {
@@ -17,16 +24,16 @@ public class Main {
 
                 switch (java.getRank()) {
                     case JUNIOR:
-                        System.out.println("I am Junior developer");
+                        System.out.println("JUNIOR: I am Junior developer");
                         break;
                     case SENIOR:
-                        System.out.println("I am Senior developer");
+                        System.out.println("SENIOR: I am Senior developer");
                         break;
                     case MIDDLE:
-                        System.out.println("I am Middle developer");
+                        System.out.println("MIDDLE: I am Middle developer");
                         break;
                     case TEAMLEAD:
-                        System.out.println("I am Teamlead developer");
+                        System.out.println("TEAMLEAD: I am Teamlead developer");
                         break;
                     default:
                         System.out.println("THE END");
--- src/child/Developer.java
+++ src/child/Developer.java
@@ -3,27 +3,38 @@ package child;
 import parent.*;
 
 public class Developer extends Employee {
-    String phrase;
+    String grades;
+    public Developer() {
 
-    public Developer(String phrase, Rank rank) {
-        super(phrase, rank);
     }
 
+    public Developer(Rank rank, String name, int age, double salary) {
+        super(rank, name, age, salary);
+    }
 
-//    public void writeCode(){
-//       // super.Work();
-//        System.out.println(" Developers - good made games.  " + " -WriteCode.  ---" + getName() +  ", РАНГ РАЗРАБОТЧИКА: " + getGrades() +
-//                ", ЗАРПЛАТА: " + getSalary() + ", ВОЗРАСТ: " + getAge());
-//    }
-//
-//    public void goToVacation(){
-//        System.out.println("Developers say:  I am going to bootcamp! ");
-//    }
+    public String getGrades() {
+        return grades;
+    }
+
+    public void setGrades(String grades) {
+        this.grades = grades;
+    }
+
+    public void writeCode(){
+        // super.Work();
+        System.out.println(" Developers - good made games.  " + " -WriteCode.  ---" + getName() +
+                ", ЗАРПЛАТА: " + getSalary() + ", ВОЗРАСТ: " + getAge());
+    }
 
     public void goToDayOff(){
         System.out.println("Разработчики, Отдохните от своих компьютеров! ");
     }
+
+    public void goToVacation(){
+        System.out.println("Developers say:  I am going to bootcamp! ");
+    }
+
     public void printDeveloperGrade(){
-        System.out.println(phrase);
+        System.out.println(getName());
     }
 }
