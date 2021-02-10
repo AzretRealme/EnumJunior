46344d077bdbe4a792bf4e3a6eca5f09696c35a9
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2021-02-10 15:21:11 +0600

    updated
    updated

--- src/child/Developer.java
+++ src/child/Developer.java
@@ -3,38 +3,56 @@ package child;
 import parent.*;
 
 public class Developer extends Employee {
-    String grades;
-    public Developer() {
 
-    }
+       private Rank rank;
+       private String name;
+       private int age;
+       private double salary;
 
-    public Developer(Rank rank, String name, int age, double salary) {
-        super(rank, name, age, salary);
-    }
+public Developer(){
 
-    public String getGrades() {
-        return grades;
-    }
-
-    public void setGrades(String grades) {
-        this.grades = grades;
-    }
-
-    public void writeCode(){
-        // super.Work();
-        System.out.println(" Developers - good made games.  " + " -WriteCode.  ---" + getName() +
-                ", ЗАРПЛАТА: " + getSalary() + ", ВОЗРАСТ: " + getAge());
-    }
-
-    public void goToDayOff(){
-        System.out.println("Разработчики, Отдохните от своих компьютеров! ");
-    }
-
-    public void goToVacation(){
-        System.out.println("Developers say:  I am going to bootcamp! ");
-    }
-
-    public void printDeveloperGrade(){
-        System.out.println(getName());
-    }
 }
+public Developer(Rank rank, String name, int age, double salary){
+    this.rank = rank;
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+
+}
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public double getSalary() {
+        return salary;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void writeCode() {
+            // super.Work();
+            System.out.println(" Developers - good made games.  " + " -WriteCode.  ---" + getName() +
+                    ", ЗАРПЛАТА: " + getSalary() + ", ВОЗРАСТ: " + getAge());
+        }
+        public void goToDayOff() {
+            System.out.println("Разработчики, Отдохните от своих компьютеров! ");
+        }
+
+        public void goToVacation() {
+            System.out.println("Developers say:  I am going to bootcamp! ");
+        }
+
+        public void printDeveloperGrade() {
+            System.out.println(getName());
+        }
+    }
