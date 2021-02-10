838f59b6247a64b01f84454c4267bcbecd7c9e5b
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2021-02-10 15:27:22 +0600

    updated
    updated

--- src/parent/Employee.java
+++ src/parent/Employee.java
@@ -2,52 +2,31 @@ package parent;
 
 public abstract class Employee {
 
-    private Rank rank;
-    String name;
-    int age;
-    double salary;
-
-    public void setRank(Rank rank) {
-        this.rank = rank;
-    }
+    
+   private String name;
+   private int age;
+   private double salary;
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public double getSalary() {
         return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 
     public Employee() {
 
     }
 
-    public Employee(Rank rank, String name, int age, double salary) {
-        this.rank = rank;
+    public Employee( String name, int age, double salary) {
         this.name = name;
         this.age = age;
         this.salary = salary;
-    }
-
-    public Rank getRank() {
-        return rank;
     }
 
     public void Work(){
